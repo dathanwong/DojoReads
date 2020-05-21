b7fdf53547dad6a195be0c5e333d2034ad601104
dathwo@outlook.com <dathwo@outlook.com>
2020-05-21 15:22:19 -0700

    Completed dojo reads assignment

---
+++ books/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from . import views
+
+#Pattern starts with /books
+urlpatterns = [
+    path('', views.books),
+    path('/add', views.addBooksPage),
+    path('/create', views.createBook),
+    path('/<int:bookID>', views.bookPage),
+    path('/<int:bookID>/addReview', views.addReview),
+    path('/<int:bookID>/<int:reviewID>/delete', views.deleteReview),
+    path('/users/<int:userID>', views.users)
+]
